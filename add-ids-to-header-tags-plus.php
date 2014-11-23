ea5ea1f31ec3b82e9965e929470e5f26e42a0e41
Chris Ferdinandi <hello@gomakethings.com>
2014-11-22 20:54:03 -0500

    Removed trailing ?>

--- add-ids-to-header-tags-plus.php
+++ add-ids-to-header-tags-plus.php
@@ -1,17 +1,17 @@
 <?php
 
-/**
- * Plugin Name: Add IDs to Header Tags
- * Plugin URI: http://github.com/cferdinandi/add-ids-to-header-tags-plus
- * Description: Add an `id` attribute to header tags in your posts. Adjust settings under <a href="options-general.php?page=addIDs_plugin_settings">Settings &rarr; Add IDs to Header Tags</a>
- * Author: Chris Ferdinandi
- * Author URI: http://gomakethings.com
- * Version: 1.2.0
- * License: MIT
- *
- * A fork of the Add IDs to Header Tags plugin.
- * http://wordpress.org/plugins/add-ids-to-header-tags/
- */
+	/**
+	 * Plugin Name: Add IDs to Header Tags
+	 * Plugin URI: http://github.com/cferdinandi/add-ids-to-header-tags-plus
+	 * Description: Add an `id` attribute to header tags in your posts. Adjust settings under <a href="options-general.php?page=addIDs_plugin_settings">Settings &rarr; Add IDs to Header Tags</a>
+	 * Author: Chris Ferdinandi
+	 * Author URI: http://gomakethings.com
+	 * Version: 1.2.0
+	 * License: MIT
+	 *
+	 * A fork of the Add IDs to Header Tags plugin.
+	 * http://wordpress.org/plugins/add-ids-to-header-tags/
+	 */
 
-require_once( dirname( __FILE__) . '/add-ids-to-header-tags-plus-options.php' );
-require_once( dirname( __FILE__) . '/add-ids-to-header-tags-plus-process.php' );
+	require_once( dirname( __FILE__) . '/add-ids-to-header-tags-plus-options.php' );
+	require_once( dirname( __FILE__) . '/add-ids-to-header-tags-plus-process.php' );
