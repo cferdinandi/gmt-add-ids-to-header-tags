c9dbdb8a7eb8d5314c4bf25b9e0be61c2d021987
cferdinandi <chris@gomakethings.com>
2016-11-03 12:26:03 -0400

    v1.4.1 Updated plugin header

--- add-ids-to-header-tags-plus.php
+++ add-ids-to-header-tags-plus.php
@@ -2,12 +2,12 @@
 
 	/**
 	 * Plugin Name: GMT Add IDs to Header Tags Plus
-	 * Plugin URI: https://github.com/cferdinandi/add-ids-to-header-tags-plus
-	 * GitHub Plugin URI: https://github.com/cferdinandi/add-ids-to-header-tags-plus
+	 * Plugin URI: https://github.com/cferdinandi/gmt-add-ids-to-header-tags-plus
+	 * GitHub Plugin URI: https://github.com/cferdinandi/gmt-add-ids-to-header-tags-plus
 	 * Description: Add an `id` attribute to header tags in your posts. Adjust settings under <a href="options-general.php?page=addIDs_plugin_settings">Settings &rarr; Add IDs to Header Tags</a>
 	 * Author: Chris Ferdinandi
 	 * Author URI: http://gomakethings.com
-	 * Version: 1.4.0
+	 * Version: 1.4.1
 	 * License: MIT
 	 *
 	 * A fork of the Add IDs to Header Tags plugin.
